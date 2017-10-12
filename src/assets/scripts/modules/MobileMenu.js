1da6dbca47fd24ae02cb4593eeef8801ca8a5e44
Vijayanand Sodadasi <vijayanand.sodadasi@gmail.com>
2017-10-11 22:39:52 -0600

    Added styling to mobile menu to display at screen centre and with background color for visibility

--- src/assets/scripts/modules/MobileMenu.js
+++ src/assets/scripts/modules/MobileMenu.js
@@ -1,6 +1,7 @@
 import $ from 'jquery';
 class MobileMenu {
     constructor() {
+        this.siteHeader = $(".site-header");
         this.menuIcon = $(".site-header__menu-icon");
         this.menuContent = $(".site-header__menu-content");
         this.events();
@@ -12,6 +13,7 @@ class MobileMenu {
 
     toggleTheMenu() {
         this.menuContent.toggleClass("site-header__menu-content--is-visible");
+        this.siteHeader.toggleClass("site-header--is-expanded");
     }
 }
 
