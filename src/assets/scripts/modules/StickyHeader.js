8547b95f07de5f828be89ef7bbcecb50bdf66d9f
Vijayanand Sodadasi <vijayanand.sodadasi@gmail.com>
2017-10-12 22:24:36 -0600

    Highlight menu item in navigation bar in the header when page is scrolled to that section

--- src/assets/scripts/modules/StickyHeader.js
+++ src/assets/scripts/modules/StickyHeader.js
@@ -1,4 +1,5 @@
 import $ from 'jquery';
+import smoothScroll from 'jquery-smooth-scroll';
 import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
 
 class StickyHeader {
@@ -7,8 +8,17 @@ class StickyHeader {
         this.primaryNav = $(".primary-nav");
         this.siteHeaderButtonContainer = $(".site-header__btn-container");
         this.headerTriggerElement = $(".large-hero__title");
+        this.pageSections = $(".page-section");
+        this.headerLinks = $(".primary-nav a");
 
         this.createHeaderWaypoint();
+        this.cratePageSectionWaypoints(); // for highlighting menu item when page is scrolled to that section
+        this.addSmoothScrolling();
+
+    }
+
+    addSmoothScrolling() {
+        this.headerLinks.smoothScroll();
     }
 
     createHeaderWaypoint() {
@@ -26,6 +36,36 @@ class StickyHeader {
                     that.siteHeaderButtonContainer.removeClass("site-header__btn-container--sticky-header")
                 }
             }
+        });
+    }
+
+    cratePageSectionWaypoints(){
+        var that = this;
+        this.pageSections.each(function() {
+            var currentPageSection = this;
+            new Waypoint({
+                element: currentPageSection,
+                handler: function(direction) {
+                    if(direction == "down") {
+                        var matchingHeaderLink = currentPageSection.getAttribute("data-matching-link");
+                        that.headerLinks.removeClass("is-current-link");
+                        $(matchingHeaderLink).addClass("is-current-link");
+                    }                    
+                },
+                offset: "18%"
+            });
+
+            new Waypoint({
+                element: currentPageSection,
+                handler: function(direction) {
+                    if(direction == "up") {
+                        var matchingHeaderLink = currentPageSection.getAttribute("data-matching-link");
+                        that.headerLinks.removeClass("is-current-link");
+                        $(matchingHeaderLink).addClass("is-current-link");
+                    }                    
+                },
+                offset: "-20%"
+            });
         });
     }
 }
